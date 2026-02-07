0e40ad1bb6c898acf0397b95fb5eee1586335820
OlegGaming <46632391+OlegGaming@users.noreply.github.com>
2026-02-07 05:01:06 +0500

    Project Setup

---
+++ Assets/CODE/GameManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using System.Collections;
+using System.Linq;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    IEnumerator Kostya()
+    {
+        yield return new WaitForSeconds(1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
